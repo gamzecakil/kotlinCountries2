e3dbb7b7b9bc1efb20e954964480f8a42632da73
gamzecakil <gmzcakil@gmail.com>
2023-10-24 18:36:27 +0300

    model olusturuldu

---
+++ app/src/main/java/com/gamzeuysal/kotlincountries2/model/model.kt
@@ -0,0 +1,11 @@
+package com.gamzeuysal.kotlincountries2.model
+
+data class Country(val countryName : String?,
+                   val countryRegion : String?,
+                   val countryCapital :String?,
+                   val counrtyCurrency :String?,
+                   val countryLanguage :String?,
+                   val imageUrl :String?)
+{
+
+}
