6680c106908d9dd846d273f17012e47cfa583c57
gamzecakil <gmzcakil@gmail.com>
2023-11-09 13:16:09 +0300

    textView'lar için data binding işlemi uygulandı.

--- app/src/main/java/com/gamzeuysal/kotlincountries2/adapter/CountryAdapter.kt
+++ app/src/main/java/com/gamzeuysal/kotlincountries2/adapter/CountryAdapter.kt
@@ -3,9 +3,11 @@ package com.gamzeuysal.kotlincountries2.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.gamzeuysal.kotlincountries2.R
+import com.gamzeuysal.kotlincountries2.databinding.ItemRowBinding
 import com.gamzeuysal.kotlincountries2.model.Country
 import com.gamzeuysal.kotlincountries2.util.dowloadFromUrl
 import com.gamzeuysal.kotlincountries2.util.placeholderProgressBar
@@ -15,16 +17,27 @@ import kotlinx.android.synthetic.main.item_row.view.*
 
 class CountryAdapter (val countyList : ArrayList<Country>) :RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
 
-    class  CountryViewHolder( view :View):RecyclerView.ViewHolder(view){
+/*
+    class  CountryViewHolder( var view :View):RecyclerView.ViewHolder(view){
 
     }
-//layout
+ */
+    //Data Binding
+class  CountryViewHolder( var view :ItemRowBinding):RecyclerView.ViewHolder(view.root) {
+
+}
+
+    //layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row,parent,false)
+        val inflater = LayoutInflater.from(parent.context)
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row,parent,false)
+        //Data Binding
+        val view  = DataBindingUtil.inflate<ItemRowBinding>(inflater,R.layout.item_row,parent,false)
         return CountryViewHolder(view)
     }
 //item
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+    /*
         holder.itemView.textName.text = countyList[position].countryName
         holder.itemView.textRegion.text = countyList[position].countryRegion
      //bir itema tıklandıgında diğer fragmenta geçelim
@@ -38,7 +51,9 @@ class CountryAdapter (val countyList : ArrayList<Country>) :RecyclerView.Adapter
 
     )
     }
-
+*/
+        //Data Binding
+        holder.view.country = countyList[position]
     }
 
     override fun getItemCount(): Int {
